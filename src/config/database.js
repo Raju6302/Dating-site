1b94db257114c386b25b3d7bb70234d305eae3e7
vijay jaripity <137879456+Raju6302@users.noreply.github.com>
2026-02-26 00:00:12 +0530

    Delete src directory

--- src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-  await mongoose.connect(
-   "mongodb+srv://vijay:DSRCWT4TbbK3GRd@cluster0.bqozzjf.mongodb.net/Dating"
-  );
-};
-
-module.exports = connectDB;
